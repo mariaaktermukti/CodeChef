e59ac52b890dae1d86a5fb58902608ae60dbf2d8
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-03-24 00:20:26 +0600

    Add files via upload

---
+++ Cred_coins.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int T;
+    cin >> T;  
+    while (T--) 
+    {
+        int X, Y;
+        cin >> X >> Y; 
+        
+        int totalCoins = X * Y;
+        
+        int bags = totalCoins / 100;
+
+        cout << bags << endl;
+    }
+    return 0;
+}
