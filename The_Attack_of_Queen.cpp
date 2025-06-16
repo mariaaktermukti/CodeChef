8afaba8bd02e315997953bf2498ce76502fb47d9
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-06-16 07:39:32 +0600

    Add files via upload

---
+++ The_Attack_of_Queen.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) 
+    {
+        int n, x, y;
+        cin >> n >> x >> y;
+        
+        int horizontal = 2 * (n - 1);
+        int diagonal = 0;
+        diagonal = diagonal+  min(x - 1, y - 1);
+        diagonal = diagonal+ min(x - 1, n - y);
+        diagonal = diagonal+ min(n - x, y - 1);
+        diagonal = diagonal+ min(n - x, n - y);
+        
+        cout << horizontal + diagonal << endl;
+    }
+    
+    return 0;
+}
