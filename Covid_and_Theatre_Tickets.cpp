7f767972c8a11159ae1c915b187a6a9c3da5df52
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-06-17 09:16:53 +0600

    Add files via upload

---
+++ Covid_and_Theatre_Tickets.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,k;
+	    cin>>n>>k;
+	    cout<< ((n+1)/2) * ((k+1)/2) <<endl;
+	}
+	return 0;
+}
