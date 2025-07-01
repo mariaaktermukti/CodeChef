8b74d5687872ec846b68dbd9e88c024e0b77c841
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-07-01 08:28:30 +0600

    Add files via upload

---
+++ Sugarcane_Juice_Business.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,n;
+    std::cin >> t;
+    while(t--)
+    {
+        std::cin >> n;
+        std::cout << n*15 << std::endl;
+    }
+	return 0;
+}
